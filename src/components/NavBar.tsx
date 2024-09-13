2deca952141d39ee4b0eba3462efd0cfcb39d7cc
Nyikwagh Moses <mosnyik@gmail.com>
2024-09-13 16:11:50 +0100

    background adjusted

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -179,7 +179,7 @@ export default function Navbar() {
                 className={`block pl-3 pr-4 py-2 border-l-4 text-base font-medium ${
                   pathname === item.href
                     ? "bg-blue-300 border-blue-500 text-gray-600"
-                    : "border-transparent text-black hover:bg-gray-50 hover:border-gray-300 hover:text-white"
+                    : "border-transparent text-black hover:bg-blue-300 hover:border-blue-500 hover:text-gray-600"
                 }`}
                 onClick={() => setMobileMenuOpen(false)}
               >
