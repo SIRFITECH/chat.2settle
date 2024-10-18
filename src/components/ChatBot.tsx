a7d2938359d9eb6fb48a259be0a40e1fdfeb8b2b
Nyikwagh Moses <mosnyik@gmail.com>
2024-10-18 14:28:55 +0100

    phone view redone

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -97,8 +97,7 @@ import {
 } from "@/helpers/api_call/reportly_page_calls";
 import { reportData } from "@/types/reportly_types";
 import ConfirmAndProceedButton from "@/hooks/confirmButtonHook";
-// import { format } from "path";
-import { format } from "date-fns";
+import { differenceInHours, format, isToday, isYesterday } from "date-fns";
 const initialMessages = [
   {
     type: "incoming",
@@ -140,7 +139,12 @@ const deserializeMessage = (message: { type: string; content: string }) => {
   };
 };
 
-const ChatBot = () => {
+interface ChatBotProps {
+  isMobile: boolean;
+  onClose: () => void;
+}
+
+const ChatBot: React.FC<ChatBotProps> = ({ isMobile, onClose }) => {
   // CONST VARIABLES
   const account = useAccount();
   const wallet = account.address;
@@ -258,6 +262,8 @@ const ChatBot = () => {
   const [fraudsterWalletAddress, setFraudsterWalletAddress] = useState("");
   const [descriptionNote, setDescriptionNote] = useState("");
   const [reportId, setReportId] = useState("");
+  const [showDateDropdown, setShowDateDropdown] = useState(false);
+  const [currentDate, setCurrentDate] = useState<string | null>(null);
 
   // REF HOOKS
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -2905,14 +2911,31 @@ const ChatBot = () => {
   //                 />
   //               </span>
   //             )}
-  //             <div
-  //               className={`p-2 md:p-3 rounded-lg ${
-  //                 msg.type === "incoming"
-  //                   ? "bg-gray-200 text-black rounded-bl-none"
-  //                   : "bg-blue-500 text-white rounded-br-none"
-  //               }`}
-  //             >
-  //               <p className="text-xs md:text-sm">{msg.content}</p>
+  //             <div className="flex flex-col max-w-[75%]">
+  //               <div
+  //                 className={`p-2 md:p-3 rounded-lg ${
+  //                   msg.type === "incoming"
+  //                     ? "bg-gray-200 text-black rounded-bl-none"
+  //                     : "bg-blue-500 text-white rounded-br-none"
+  //                 }`}
+  //               >
+  //                 <p className="text-xs md:text-sm">{msg.content}</p>
+  //               </div>
+  //               <span
+  //                 className={`text-xs text-gray-500 mt-1 ${
+  //                   msg.type === "incoming" ? "self-end" : "self-start"
+  //                 }`}
+  //                 aria-label={`Sent at ${format(
+  //                   new Date(msg.timestamp),
+  //                   "h:mm a"
+  //                 )}`}
+  //               >
+  //                 {
+  //                   <div className="text-xs">
+  //                     {format(new Date(msg.timestamp), "h:mm a").toLowerCase()}
+  //                   </div>
+  //                 }
+  //               </span>
   //             </div>
   //           </li>
   //         ))}
@@ -2949,90 +2972,163 @@ const ChatBot = () => {
   //           onChange={(e) => setChatInput(e.target.value)}
   //           onKeyDown={handleKeyPress}
   //           style={{
-  //             lineHeight: "40px", // This matches the height of the textarea
+  //             lineHeight: "40px",
   //             paddingTop: "0",
   //             paddingBottom: "0",
   //           }}
   //         />
-  //         <span
+  //         <button
   //           className="ml-2 text-blue-500 cursor-pointer material-symbols-rounded"
   //           onClick={() => handleConversation(chatInput)}
+  //           aria-label="Send message"
   //         >
   //           <SendIcon />
-  //         </span>
+  //         </button>
   //       </div>
   //     )}
   //   </div>
   // );
+  const renderDateSeparator = (date: Date) => {
+    if (isToday(date)) {
+      return "Today";
+    } else if (isYesterday(date)) {
+      return "Yesterday";
+    } else if (differenceInHours(new Date(), date) <= 48) {
+      return format(date, "MMMM d, yyyy");
+    } else {
+      return format(date, "EEEE"); // Returns the full name of the day (e.g., Monday, Tuesday)
+    }
+  };
+
+  useEffect(() => {
+    if (chatMessages.length > 0) {
+      const latestMessage = chatMessages[chatMessages.length - 1];
+      const dateString = renderDateSeparator(new Date(latestMessage.timestamp));
+      setCurrentDate(dateString);
+      setShowDateDropdown(true);
+
+      const timer = setTimeout(() => {
+        setShowDateDropdown(false);
+      }, 15000); // 15 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [chatMessages]);
+
+  const groupedMessages = chatMessages.reduce((groups, message) => {
+    const date = new Date(message.timestamp);
+    const dateString = format(date, "yyyy-MM-dd");
+    if (!groups[dateString]) {
+      groups[dateString] = [];
+    }
+    groups[dateString].push(message);
+    return groups;
+  }, {} as Record<string, MessageType[]>);
+
   return (
     <div
       ref={chatboxRef}
-      className="fixed right-8 bottom-24 w-10/12 md:w-7/12 lg:w-6/12 bg-white rounded-lg shadow-lg overflow-hidden transform transition-all duration-300 ease-in-out"
-      style={{ maxHeight: "80vh", display: "flex", flexDirection: "column" }}
+      className={`fixed ${
+        isMobile ? "inset-0" : "right-8 bottom-24 w-10/12 md:w-7/12 lg:w-6/12"
+      } bg-white rounded-lg shadow-lg overflow-hidden flex flex-col`}
+      style={{ height: isMobile ? "100%" : "80vh" }}
     >
-      <header className="py-4 text-center text-white bg-blue-500 shadow">
-        <div className="flex items-center justify-between relative">
-          <span className="flex-shrink-0 w-8 h-8 ml-8 bg-white rounded">
+      <header className="py-4 text-center text-white bg-blue-500 shadow relative z-10">
+        <div className="flex items-center justify-between px-4">
+          <span className="flex-shrink-0 w-8 h-8 bg-white rounded">
             <Image
               src="/waaa.png"
               alt="Avatar"
-              width={500}
-              height={100}
-              className="w-full h-full rounded"
+              width={32}
+              height={32}
+              className="rounded"
             />
           </span>
-          <h2 className="text-lg font-bold absolute left-1/2 transform -translate-x-1/2">
-            2SettleHQ
-          </h2>
-        </div>
-      </header>
-      {isOpen && (
-        <ul className="flex-grow p-4 md:p-8 space-y-4 overflow-y-auto">
-          {chatMessages.map((msg, index) => (
-            <li
-              key={index}
-              className={`flex ${
-                msg.type === "incoming" ? "items-start" : "justify-end"
-              }`}
+          <h2 className="text-lg font-bold">2SettleHQ</h2>
+          {isMobile && (
+            <button
+              onClick={onClose}
+              className="text-white"
+              aria-label="Close chat"
             >
-              {msg.type === "incoming" && (
-                <span className="flex-shrink-0 w-6 h-6 md:w-8 md:h-8 mr-2 md:mr-4 bg-white rounded self-end">
-                  <Image
-                    src="/waaa.png"
-                    alt="Avatar"
-                    width={500}
-                    height={100}
-                    className="w-full h-full rounded"
-                  />
-                </span>
-              )}
-              <div className="flex flex-col max-w-[75%]">
-                <div
-                  className={`p-2 md:p-3 rounded-lg ${
-                    msg.type === "incoming"
-                      ? "bg-gray-200 text-black rounded-bl-none"
-                      : "bg-blue-500 text-white rounded-br-none"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+        {showDateDropdown && currentDate && (
+          <div
+            className="absolute top-full left-1/2 transform -translate-x-1/2 bg-gray-200 text-gray-700 px-4 py-2 rounded-b-lg shadow-md text-sm transition-all duration-500 ease-in-out"
+            style={{
+              animation: "dropDown 0.5s ease-out",
+            }}
+          >
+            {currentDate}
+          </div>
+        )}
+      </header>
+      <div className="flex-grow overflow-y-auto">
+        <ul className="p-4 md:p-8 space-y-4">
+          {Object.entries(groupedMessages).map(([dateString, messages]) => (
+            <React.Fragment key={dateString}>
+              <li className="text-center text-sm text-gray-500 my-2">
+                {renderDateSeparator(new Date(dateString))}
+              </li>
+              {messages.map((msg, index) => (
+                <li
+                  key={`${dateString}-${index}`}
+                  className={`flex ${
+                    msg.type === "incoming" ? "items-start" : "justify-end"
                   }`}
                 >
-                  <p className="text-xs md:text-sm">{msg.content}</p>
-                </div>
-                <span
-                  className={`text-xs text-gray-500 mt-1 ${
-                    msg.type === "incoming" ? "self-end" : "self-start"
-                  }`}
-                  aria-label={`Sent at ${format(
-                    new Date(msg.timestamp),
-                    "h:mm a"
-                  )}`}
-                >
-                  {
-                    <div className="text-xs">
-                      {format(new Date(msg.timestamp), "h:mm a")}
+                  {msg.type === "incoming" && (
+                    <span className="flex-shrink-0 w-6 h-6 md:w-8 md:h-8 mr-2 md:mr-4 bg-white rounded self-end">
+                      <Image
+                        src="/waaa.png"
+                        alt="Avatar"
+                        width={32}
+                        height={32}
+                        className="rounded"
+                      />
+                    </span>
+                  )}
+                  <div className="flex flex-col max-w-[75%]">
+                    <div
+                      className={`p-2 md:p-3 rounded-lg ${
+                        msg.type === "incoming"
+                          ? "bg-gray-200 text-black rounded-bl-none"
+                          : "bg-blue-500 text-white rounded-br-none"
+                      }`}
+                    >
+                      <p className="text-xs md:text-sm">{msg.content}</p>
                     </div>
-                  }
-                </span>
-              </div>
-            </li>
+                    <span
+                      className={`text-xs text-gray-500 mt-1 ${
+                        msg.type === "incoming" ? "self-end" : "self-start"
+                      }`}
+                      aria-label={`Sent at ${format(
+                        new Date(msg.timestamp),
+                        "h:mm a"
+                      )}`}
+                    >
+                      {format(new Date(msg.timestamp), "h:mm a").toLowerCase()}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </React.Fragment>
           ))}
           {loading && (
             <div className="flex items-center">
@@ -3040,9 +3136,9 @@ const ChatBot = () => {
                 <Image
                   src="/waaa.png"
                   alt="Avatar"
-                  width={200}
-                  height={100}
-                  className="w-full h-full rounded"
+                  width={32}
+                  height={32}
+                  className="rounded"
                 />
               </span>
               <div className="bg-gray-200 relative left-1 top-1 rounded-bl-none pr-2 pt-2 pl-2 pb-1 md:pr-4 md:pt-4 md:pl-3 md:pb-2 rounded-lg mr-12 md:mr-48">
@@ -3054,33 +3150,29 @@ const ChatBot = () => {
           )}
           <div ref={messagesEndRef} />
         </ul>
-      )}
-      {isOpen && (
-        <div className="flex items-center p-3 border-t border-gray-200 bg-white pr-4">
+      </div>
+      <div className="p-3 border-t border-gray-200 bg-white">
+        <div className="flex items-center">
           <textarea
             ref={textareaRef}
-            className="flex-1 pl-2 border-none outline-none resize-none h-10"
+            className="flex-grow pl-2 pr-2 py-2 border-none outline-none resize-none"
             placeholder="Enter a message..."
+            rows={1}
             spellCheck={false}
             required
             value={chatInput}
             onChange={(e) => setChatInput(e.target.value)}
             onKeyDown={handleKeyPress}
-            style={{
-              lineHeight: "40px",
-              paddingTop: "0",
-              paddingBottom: "0",
-            }}
           />
           <button
-            className="ml-2 text-blue-500 cursor-pointer material-symbols-rounded"
+            className="ml-2 text-blue-500 cursor-pointer"
             onClick={() => handleConversation(chatInput)}
             aria-label="Send message"
           >
             <SendIcon />
           </button>
         </div>
-      )}
+      </div>
     </div>
   );
 };
