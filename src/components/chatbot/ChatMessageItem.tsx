e64902757e0c4bf93c0e19a928e20e06a33a9b10
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-20 18:37:53 +0100

    Refactor: Extracted ChatHeader, ChatInput, ChatMessageItem and ChatMessages into reusable components for readeabilty

---
+++ src/components/chatbot/ChatMessageItem.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+import { MessageType } from "@/types/general_types";
+import { format } from "date-fns";
+interface Props {
+  msg: MessageType;
+  dateString: string;
+  index: number;
+}
+
+const ChatMessageItem = ({ msg, dateString, index }: Props) => {
+  return (
+    <li
+      key={`${dateString}-${index}`}
+      className={`flex ${
+        msg.type === "incoming" ? "items-start" : "justify-end"
+      }`}
+    >
+      {msg.type === "incoming" && (
+        <span className="flex-shrink-0 w-6 h-6 md:w-8 md:h-8 mr-2 md:mr-4 bg-white rounded self-end">
+          <Image
+            src="/waaa.png"
+            alt="Avatar"
+            width={32}
+            height={32}
+            className="rounded"
+          />
+        </span>
+      )}
+      <div className="flex flex-col max-w-[75%]">
+        <div
+          className={`p-2 md:p-3 rounded-lg ${
+            msg.type === "incoming"
+              ? "bg-gray-200 text-black rounded-bl-none"
+              : "bg-blue-500 text-white rounded-br-none"
+          }`}
+        >
+          <p className="text-xs md:text-sm">{msg.content}</p>
+        </div>
+        <span
+          className={`text-xs text-gray-500 mt-1 ${
+            msg.type === "incoming" ? "self-end" : "self-start"
+          }`}
+        >
+          {format(new Date(msg.timestamp), "h:mm a").toLowerCase()}
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default ChatMessageItem;
