5997c4e8b3911b1d91c2fd72bc08bda54a9c9dc1
Nyikwagh Moses <mosnyik@gmail.com>
2025-05-10 04:58:16 +0100

    Adjusted date on the dashboard display

--- src/components/shared/DashboardFilteredTransactions.tsx
+++ src/components/shared/DashboardFilteredTransactions.tsx
@@ -16,19 +16,19 @@ const DashboardFilteredTransactions = ({ filteredTransactions }: Props) => {
     .slice(0, 20)
     .map((transaction, index) => (
       <TableRow key={index} className="text-black">
-        <TableCell className="hidden md:table-cell">
+        <TableCell >
           {transaction.Date || "N/A"}
         </TableCell>
         <TableCell className="hidden md:table-cell">
           {transaction.transac_id || "N/A"}
         </TableCell>
-        <TableCell>
+        <TableCell >
           {transaction.receiver_amount || "N/A"}
           {/* {formatCurrency(transaction.receiver_amount || "0", "NGN", "en-NG") ||
             "N/A"} */}
         </TableCell>
         <TableCell>{transaction.crypto_sent || "N/A"}</TableCell>
-        <TableCell>{transaction.current_rate || "N/A"}</TableCell>
+        <TableCell className="hidden md:table-cell" >{transaction.current_rate || "N/A"}</TableCell>
         <TableCell className="hidden md:table-cell">
           {transaction.charges || "N/A"}
         </TableCell>
