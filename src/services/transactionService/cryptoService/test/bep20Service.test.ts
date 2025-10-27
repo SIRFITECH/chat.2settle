a3842b838c7c255adf36491697438daadf6cb8ac
Nyikwagh Moses <mosnyik@gmail.com>
2025-10-27 17:02:43 +0100

    feat: added test for usdt bep20

---
+++ src/services/transactionService/cryptoService/test/bep20Service.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "vitest";
+
+describe("bep20Service", () => {
+  it("should throw error if wallet is not connected while calling the function", () => {
+    //arrange
+    // act
+    //assert
+  });
+});
