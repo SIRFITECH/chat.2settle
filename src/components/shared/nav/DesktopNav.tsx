c4327b0a7986a0fd789bebd797657065e322245b
Nyikwagh Moses <mosnyik@gmail.com>
2026-02-16 05:32:12 +0100

    fix: wallet show fixed

--- src/components/shared/nav/DesktopNav.tsx
+++ src/components/shared/nav/DesktopNav.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "./NavLink";
 
 export function DesktopNav() {
   return (
-    <div className="hidden sm:flex space-x-4 md:space-x-8">
+    <div className="hidden lg:flex space-x-4 lg:space-x-6 xl:space-x-8">
       {NAV_ITEMS.map((item) => (
         <NavLink key={item.label} href={item.href}>
           {item.label}
